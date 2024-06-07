ca6998b7cce33efb85408eeb5700c9a67acc2408
hben35 <99895665+hben35@users.noreply.github.com>
2024-06-07 15:57:55 +0200

    Update settings.py

--- lama_cleaner/settings.py
+++ lama_cleaner/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-=x2n@zasb2nkq$)frp(&h*tsozyka+jb5(&3^7@u5@ven@-sdu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'lama.cap.dev.processflow.fr',
+    # Ajoutez d'autres URL si nécessaire
+]
 
 
 # Application definition
