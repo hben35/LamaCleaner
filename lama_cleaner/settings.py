2dbc75fa79cd6d40da11c38c66610038b88821e7
hben35 <99895665+hben35@users.noreply.github.com>
2024-06-07 16:59:33 +0200

    Update settings.py

--- lama_cleaner/settings.py
+++ lama_cleaner/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=x2n@zasb2nkq$)frp(&h*tsozyka+jb5(&3^7@u5@ven@-sdu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = false
 
 ALLOWED_HOSTS = ['app.cap.dev.processflow.fr']
 
