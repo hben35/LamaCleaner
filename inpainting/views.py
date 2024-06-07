aeb485eac310c3a22c25f3857bf3fb4b35eaa0a7
hben35 <99895665+hben35@users.noreply.github.com>
2024-06-07 17:31:56 +0200

    Update views.py

--- inpainting/views.py
+++ inpainting/views.py
@@ -1,39 +1,58 @@
+from django.shortcuts import render
+
+
 from django.http import JsonResponse
+from rest_framework.response import Response
 from rest_framework.decorators import api_view
+
+import datetime
+
+from pathlib import Path
+
 import cv2
+import pytest
+import torch
+
 from lama_cleaner.model_manager import ModelManager
-from lama_cleaner.schema import Config, HDStrategy, LDMSampler
-import os
+from lama_cleaner.schema import Config, HDStrategy, LDMSampler, SDSampler
+
+# Create your views here.
+
+
+current_dir = Path(__file__).parent.absolute().resolve()
+save_dir = current_dir / 'inpaintingresult'
+save_dir.mkdir(exist_ok=True, parents=True)
+
 
 @api_view(['POST',])
 def lamaCleaner(request):
     if request.method == "POST":
-        input_image = request.FILES['input_image']
-        mask_image = request.FILES['mask_image']
-        userid = request.POST['userid']
+		print("POST data:", request.POST)
+        print("FILES data:", request.FILES)
+        input_image = request.POST['input_image']
+        mask_image = request.POST['mask_image']
+        userid=request.POST['userid']
 
-        imagename = f"inpaint_{userid}.png"
+        imagename=f"inpaint_{userid}.png"
         model = ModelManager(name="lama", device="cpu")
-        img = cv2.imread(input_image)  # Utilisez input_image comme chemin vers le fichier
+        img = cv2.imread(str(input_image))
         img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
-        mask = cv2.imread(mask_image, cv2.IMREAD_GRAYSCALE)  # Utilisez mask_image comme chemin vers le fichier
+        mask = cv2.imread(str(mask_image), cv2.IMREAD_GRAYSCALE)
 
-        res = model(img, mask, get_config(HDStrategy.RESIZE))  # Vous pouvez utiliser trois types de stratégies (CROP, RESIZE, ORIGINAL)
-        save_dir = "/app/lama-data"  # Utilisez le répertoire persistant créé avec CapRover
-        os.makedirs(save_dir, exist_ok=True)  # Assurez-vous que le répertoire existe
+        res = model(img, mask, get_config(HDStrategy.RESIZE))  #you can use three types of strategy (CROP,RESIZE,ORIGINAL)
         cv2.imwrite(
-            f"{save_dir}/{imagename}",
+            str(save_dir / imagename),
             res,
             [int(cv2.IMWRITE_JPEG_QUALITY), 100, int(cv2.IMWRITE_PNG_COMPRESSION), 0],
         )
 
         response = {
-            'status': 200,
-            'message': "success",
-            'User': imagename
-        }
+                    'status':200,
+                    'message': "success",
+                    'User':imagename
+                    }
 
-        return JsonResponse(response, safe=False)
+        return JsonResponse(response,safe=False)
 
 def get_config(strategy, **kwargs):
     data = dict(
