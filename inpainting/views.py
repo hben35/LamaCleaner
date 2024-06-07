63fcc95cdbecf8f85e5aed016fbd161940327b68
hben35 <99895665+hben35@users.noreply.github.com>
2024-06-07 17:36:44 +0200

    Update views.py

--- inpainting/views.py
+++ inpainting/views.py
@@ -1,67 +1,27 @@
 from django.shortcuts import render
-
-
 from django.http import JsonResponse
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
-import datetime
-
 from pathlib import Path
-
 import cv2
-import pytest
-import torch
 
-from lama_cleaner.model_manager import ModelManager
-from lama_cleaner.schema import Config, HDStrategy, LDMSampler, SDSampler
+# Chemin vers les fichiers locaux
+input_image_path = Path("/chemin/vers/le/repertoire/testimages/image1.jpg")
+mask_image_path = Path("/chemin/vers/le/repertoire/testimages/mask1.jpg")
 
-# Create your views here.
-
-
-current_dir = Path(__file__).parent.absolute().resolve()
-save_dir = current_dir / 'inpaintingresult'
-save_dir.mkdir(exist_ok=True, parents=True)
-
-
-@api_view(['POST',])
+# Fonction de traitement de la requête
+@api_view(['POST'])
 def lamaCleaner(request):
     if request.method == "POST":
-		print("POST data:", request.POST)
-        print("FILES data:", request.FILES)
-        input_image = request.POST['input_image']
-        mask_image = request.POST['mask_image']
-        userid=request.POST['userid']
+        # Utilisation des fichiers locaux au lieu de request.POST
+        img = cv2.imread(str(input_image_path))
+        mask = cv2.imread(str(mask_image_path), cv2.IMREAD_GRAYSCALE)
 
-        imagename=f"inpaint_{userid}.png"
-        model = ModelManager(name="lama", device="cpu")
-        img = cv2.imread(str(input_image))
-        img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
-        mask = cv2.imread(str(mask_image), cv2.IMREAD_GRAYSCALE)
+        # Votre logique de traitement ici
 
-        res = model(img, mask, get_config(HDStrategy.RESIZE))  #you can use three types of strategy (CROP,RESIZE,ORIGINAL)
-        cv2.imwrite(
-            str(save_dir / imagename),
-            res,
-            [int(cv2.IMWRITE_JPEG_QUALITY), 100, int(cv2.IMWRITE_PNG_COMPRESSION), 0],
-        )
-
+        # Réponse JSON de test
         response = {
-                    'status':200,
-                    'message': "success",
-                    'User':imagename
-                    }
-
-        return JsonResponse(response,safe=False)
-
-def get_config(strategy, **kwargs):
-    data = dict(
-        ldm_steps=1,
-        ldm_sampler=LDMSampler.plms,
-        hd_strategy=strategy,
-        hd_strategy_crop_margin=32,
-        hd_strategy_crop_trigger_size=200,
-        hd_strategy_resize_limit=200,
-    )
-    data.update(**kwargs)
-    return Config(**data)
+            'status': 200,
+            'message': "success",
+            'User': "Nom_de_l'utilisateur"  # Remplacer par le nom d'utilisateur approprié
+        }
+        return JsonResponse(response, safe=False)
