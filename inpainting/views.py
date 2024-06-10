ce6ebd1979c40dff2846f87a11e0a3f7d78008ea
hben35 <99895665+hben35@users.noreply.github.com>
2024-06-10 13:50:19 +0200

    Update views.py

--- inpainting/views.py
+++ inpainting/views.py
@@ -1,70 +1,56 @@
 from django.shortcuts import render
+
+
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+
 import datetime
+
 from pathlib import Path
+
 import cv2
 import pytest
 import torch
+
 from lama_cleaner.model_manager import ModelManager
 from lama_cleaner.schema import Config, HDStrategy, LDMSampler, SDSampler
 
 # Create your views here.
 
+
 current_dir = Path(__file__).parent.absolute().resolve()
 save_dir = current_dir / 'inpaintingresult'
 save_dir.mkdir(exist_ok=True, parents=True)
 
+
 @api_view(['POST',])
 def lamaCleaner(request):
     if request.method == "POST":
-        try:
-            input_image = request.POST['input_image']
-            mask_image = request.POST['mask_image']
-            userid = request.POST['userid']
+        input_image = request.POST['input_image']
+        mask_image = request.POST['mask_image']
+        userid=request.POST['userid']
 
-            imagename = f"inpaint_{userid}.png"
-            model = ModelManager(name="lama", device="cpu")
+        imagename=f"inpaint_{userid}.png"
+        model = ModelManager(name="lama", device="cpu")
+        img = cv2.imread(str(input_image))
+        img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
+        mask = cv2.imread(str(mask_image), cv2.IMREAD_GRAYSCALE)
 
-            # Vérifiez si les chemins d'images sont corrects
-            print(f"Input image path: {input_image}")
-            print(f"Mask image path: {mask_image}")
+        res = model(img, mask, get_config(HDStrategy.RESIZE))  #you can use three types of strategy (CROP,RESIZE,ORIGINAL)
+        cv2.imwrite(
+            str(save_dir / imagename),
+            res,
+            [int(cv2.IMWRITE_JPEG_QUALITY), 100, int(cv2.IMWRITE_PNG_COMPRESSION), 0],
+        )
 
-            img = cv2.imread(str(input_image))
-            if img is None:
-                print("Failed to read input image")
-            else:
-                print("Input image read successfully")
+        response = {
+                    'status':200,
+                    'message': "success",
+                    'User':imagename
+                    }
 
-            img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
-            mask = cv2.imread(str(mask_image), cv2.IMREAD_GRAYSCALE)
-            if mask is None:
-                print("Failed to read mask image")
-            else:
-                print("Mask image read successfully")
-
-            res = model(img, mask, get_config(HDStrategy.RESIZE))  # you can use three types of strategy (CROP, RESIZE, ORIGINAL)
-
-            cv2.imwrite(
-                str(save_dir / imagename),
-                res,
-                [int(cv2.IMWRITE_JPEG_QUALITY), 100, int(cv2.IMWRITE_PNG_COMPRESSION), 0],
-            )
-
-            response = {
-                'status': 200,
-                'message': "success",
-                'User': imagename
-            }
-            return JsonResponse(response, safe=False)
-
-        except Exception as e:
-            response = {
-                'status': 500,
-                'message': str(e)
-            }
-            return JsonResponse(response, safe=False)
+        return JsonResponse(response,safe=False)
 
 def get_config(strategy, **kwargs):
     data = dict(
@@ -77,3 +63,4 @@ def get_config(strategy, **kwargs):
     )
     data.update(**kwargs)
     return Config(**data)
+
