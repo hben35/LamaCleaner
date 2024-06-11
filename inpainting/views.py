6ef755af3d719760aa9d6ec60b498c851d41708d
hben35 <99895665+hben35@users.noreply.github.com>
2024-06-11 15:50:26 +0200

    Update views.py

--- inpainting/views.py
+++ inpainting/views.py
@@ -1,18 +1,15 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
-
 from pathlib import Path
-import cv2
-import requests
-import numpy as np
-import base64
 from io import BytesIO
 from PIL import Image
+import requests
+import numpy as np
+import cv2
+import base64
 
 from lama_cleaner.model_manager import ModelManager
 from lama_cleaner.schema import Config, HDStrategy, LDMSampler, SDSampler
-
 
 @api_view(['POST'])
 def lamaCleaner(request):
@@ -27,26 +24,23 @@ def lamaCleaner(request):
 
             model = ModelManager(name="lama", device="cpu")
 
-            # Télécharger et lire les images depuis les URL fournies
             img = url_to_image(input_image_url)
             if img is None:
-                return JsonResponse({'status': 400, 'message': 'Failed to download or read input image'}, safe=False)
+                return JsonResponse({'status': 400, 'message': 'Failed to download or decode input image'}, safe=False)
 
             mask = url_to_image(mask_image_url, gray=True)
             if mask is None:
-                return JsonResponse({'status': 400, 'message': 'Failed to download or read mask image'}, safe=False)
+                return JsonResponse({'status': 400, 'message': 'Failed to download or decode mask image'}, safe=False)
 
-            # Effectuer l'inpainting
-            res = model(img, mask, get_config(HDStrategy.CROP))
+            res = model(img, mask, get_config(HDStrategy.RESIZE))
 
             # Convertir l'image résultante en base64
-            _, buffer = cv2.imencode('.png', res)
-            image_base64 = base64.b64encode(buffer).decode('utf-8')
+            image_base64 = image_to_base64(res)
 
             response = {
                 'status': 200,
                 'message': "success",
-                'image_base64': "data:image/png;base64," + image_base64
+                'image_base64': image_base64
             }
 
             return JsonResponse(response, safe=False)
@@ -58,32 +52,41 @@ def lamaCleaner(request):
             }
             return JsonResponse(response, safe=False)
 
-
 def url_to_image(url, gray=False):
     """
-    Télécharge une image depuis une URL et la convertit en un format OpenCV.
+    Télécharge une image depuis une URL et la convertit en une image PIL.
     """
     try:
         response = requests.get(url)
         response.raise_for_status()
-        image_array = np.asarray(bytearray(response.content), dtype="uint8")
-        image = cv2.imdecode(image_array, cv2.IMREAD_GRAYSCALE if gray else cv2.IMREAD_COLOR)
-        if image is not None and not gray:
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = Image.open(BytesIO(response.content))
+        if gray:
+            image = image.convert('L')
         return image
     except Exception as e:
-        print(f"Error downloading image from {url}: {e}")
+        print(f"Error downloading or decoding image from {url}: {e}")
         return None
 
+def image_to_base64(image):
+    """
+    Convertit une image PIL en base64.
+    """
+    try:
+        buffered = BytesIO()
+        image.save(buffered, format="PNG")
+        return base64.b64encode(buffered.getvalue()).decode('utf-8')
+    except Exception as e:
+        print(f"Error converting image to base64: {e}")
+        return None
 
 def get_config(strategy, **kwargs):
     data = dict(
-        ldm_steps=10,
-        ldm_sampler=LDMSampler.ddim,
+        ldm_steps=1,
+        ldm_sampler=LDMSampler.plms,
         hd_strategy=strategy,
         hd_strategy_crop_margin=32,
-        hd_strategy_crop_trigger_size=512,
-        hd_strategy_resize_limit=1024,
+        hd_strategy_crop_trigger_size=200,
+        hd_strategy_resize_limit=200,
     )
     data.update(**kwargs)
     return Config(**data)
