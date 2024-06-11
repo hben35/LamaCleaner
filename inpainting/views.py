b9db515d51cf22760ec5ed474c3b078df6175e3e
hben35 <99895665+hben35@users.noreply.github.com>
2024-06-11 16:41:21 +0200

    Update views.py

--- inpainting/views.py
+++ inpainting/views.py
@@ -1,14 +1,18 @@
+from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
+
 from pathlib import Path
-from io import BytesIO
-from PIL import Image
+import cv2
 import requests
 import numpy as np
-import cv2
 import base64
+from io import BytesIO
+from PIL import Image
 
+from lama_cleaner.model_manager import ModelManager
 from lama_cleaner.schema import Config, HDStrategy, LDMSampler, SDSampler
+
 
 @api_view(['POST'])
 def lamaCleaner(request):
@@ -21,27 +25,28 @@ def lamaCleaner(request):
             if not input_image_url or not mask_image_url or not userid:
                 return JsonResponse({'status': 400, 'message': 'Missing required fields'}, safe=False)
 
+            model = ModelManager(name="lama", device="cpu")
+
+            # Télécharger et lire les images depuis les URL fournies
             img = url_to_image(input_image_url)
             if img is None:
-                return JsonResponse({'status': 400, 'message': 'Failed to download or decode input image'}, safe=False)
+                return JsonResponse({'status': 400, 'message': 'Failed to download or read input image'}, safe=False)
 
-            mask = url_to_image(mask_image_url)
+            mask = url_to_image(mask_image_url, gray=True)
             if mask is None:
-                return JsonResponse({'status': 400, 'message': 'Failed to download or decode mask image'}, safe=False)
+                return JsonResponse({'status': 400, 'message': 'Failed to download or read mask image'}, safe=False)
 
-            # Assurer que l'image et le masque ont les mêmes dimensions
-            img, mask = resize_to_same_dimension(img, mask)
-
-            # Utiliser le modèle cv2 pour l'inpainting
-            res = cv2_inpainting(img, mask)
+            # Effectuer l'inpainting
+            res = model(img, mask, get_config(HDStrategy.CROP))
 
             # Convertir l'image résultante en base64
-            image_base64 = image_to_base64(res)
+            _, buffer = cv2.imencode('.png', res)
+            image_base64 = base64.b64encode(buffer).decode('utf-8')
 
             response = {
                 'status': 200,
                 'message': "success",
-                'image_base64': image_base64
+                'image_base64': "data:image/png;base64," + image_base64
             }
 
             return JsonResponse(response, safe=False)
@@ -53,47 +58,32 @@ def lamaCleaner(request):
             }
             return JsonResponse(response, safe=False)
 
+
 def url_to_image(url, gray=False):
     """
-    Télécharge une image depuis une URL et la convertit en un format numpy.
+    Télécharge une image depuis une URL et la convertit en un format OpenCV.
     """
     try:
         response = requests.get(url)
         response.raise_for_status()
-        image = np.array(Image.open(BytesIO(response.content)))
-
-        # Convertir en niveaux de gris si demandé
-        if gray and len(image.shape) == 3:  # Assurer que l'image est en couleur
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
+        image_array = np.asarray(bytearray(response.content), dtype="uint8")
+        image = cv2.imdecode(image_array, cv2.IMREAD_GRAYSCALE if gray else cv2.IMREAD_COLOR)
+        if image is not None and not gray:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         return image
     except Exception as e:
         print(f"Error downloading image from {url}: {e}")
         return None
 
 
-def resize_to_same_dimension(img, mask):
-    """
-    Redimensionne l'image et le masque pour avoir les mêmes dimensions.
-    """
-    min_height = min(img.shape[0], mask.shape[0])
-    min_width = min(img.shape[1], mask.shape[1])
-    img = cv2.resize(img, (min_width, min_height))
-    mask = cv2.resize(mask, (min_width, min_height))
-    return img, mask
-
-def cv2_inpainting(img, mask):
-    """
-    Utilise le modèle cv2 pour effectuer l'inpainting.
-    """
-    res = cv2.inpaint(img, mask, inpaintRadius=3, flags=cv2.INPAINT_TELEA)
-    return res
-
-def image_to_base64(image):
-    """
-    Convertit une image numpy en format base64.
-    """
-    pil_image = Image.fromarray(image)
-    buffered = BytesIO()
-    pil_image.save(buffered, format="PNG")
-    return base64.b64encode(buffered.getvalue()).decode('utf-8')
+def get_config(strategy, **kwargs):
+    data = dict(
+        ldm_steps=10,
+        ldm_sampler=LDMSampler.ddim,
+        hd_strategy=strategy,
+        hd_strategy_crop_margin=32,
+        hd_strategy_crop_trigger_size=512,
+        hd_strategy_resize_limit=1024,
+    )
+    data.update(**kwargs)
+    return Config(**data)
