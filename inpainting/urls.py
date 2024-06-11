783a94d3824961e998416af1da4ba121c9d88b31
hben35 <99895665+hben35@users.noreply.github.com>
2024-06-11 18:11:43 +0200

    Update urls.py

--- inpainting/urls.py
+++ inpainting/urls.py
@@ -1,12 +1,5 @@
 from django.urls import include, path
 from inpainting.views import *
-from django.conf import settings
-from django.conf.urls.static import static
-
-# ... your existing urlpatterns
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 app_name = 'inpainting'
