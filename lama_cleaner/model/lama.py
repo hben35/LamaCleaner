b5d0b7590731373577529b7f0028a8bc19c792db
hben35 <99895665+hben35@users.noreply.github.com>
2024-06-11 14:27:22 +0200

    Update lama.py

--- lama_cleaner/model/lama.py
+++ lama_cleaner/model/lama.py
@@ -1,42 +1,46 @@
 import os
+
 import cv2
 import numpy as np
 import torch
 from loguru import logger
 
-from lama_cleaner.helper import (
-    norm_img,
-    get_cache_path_by_url,
-    load_jit_model,
-    download_model,
-)
-from lama_cleaner.schema import InpaintRequest
+from lama_cleaner.helper import pad_img_to_modulo, download_model, norm_img, get_cache_path_by_url
 from lama_cleaner.model.base import InpaintModel
+from lama_cleaner.schema import Config
 
 LAMA_MODEL_URL = os.environ.get(
     "LAMA_MODEL_URL",
     "https://github.com/Sanster/models/releases/download/add_big_lama/big-lama.pt",
 )
-LAMA_MODEL_MD5 = os.environ.get("LAMA_MODEL_MD5", "e3aa4aaa15225a33ec84f9f4bc47e500")
+
+#"https://drive.google.com/file/d/1bMD06F9hkkS1oi8cEmb4cSjXz54Pxs6A/view?usp=sharing"   #big-lama.pt file
+
 
 class LaMa(InpaintModel):
-    name = "lama"
     pad_mod = 8
-    is_erase_model = True
-
-    @staticmethod
-    def download():
-        download_model(LAMA_MODEL_URL, LAMA_MODEL_MD5)
 
     def init_model(self, device, **kwargs):
-        self.model = load_jit_model(LAMA_MODEL_URL, device, LAMA_MODEL_MD5).eval()
-        self.device = device
+        if os.environ.get("LAMA_MODEL"):
+            model_path = os.environ.get("LAMA_MODEL")
+            if not os.path.exists(model_path):
+                raise FileNotFoundError(
+                    f"lama torchscript model not found: {model_path}"
+                )
+        else:
+            model_path = download_model(LAMA_MODEL_URL)
+        logger.info(f"Load LaMa model from: {model_path}")
+        model = torch.jit.load(model_path, map_location="cpu")
+        model = model.to(device)
+        model.eval()
+        self.model = model
+        self.model_path = model_path
 
     @staticmethod
     def is_downloaded() -> bool:
         return os.path.exists(get_cache_path_by_url(LAMA_MODEL_URL))
 
-    def forward(self, image, mask, config: InpaintRequest):
+    def forward(self, image, mask, config: Config):
         """Input image and output image have same size
         image: [H, W, C] RGB
         mask: [H, W]
