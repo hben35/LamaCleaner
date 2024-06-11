8acbb6ea05c43e03e1934377b4e71f46de9dc97f
hben35 <99895665+hben35@users.noreply.github.com>
2024-06-11 18:15:03 +0200

    Update urls.py

--- lama_cleaner/urls.py
+++ lama_cleaner/urls.py
@@ -18,12 +18,11 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
-# ... your existing urlpatterns
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('inpainting/',include('inpainting.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
